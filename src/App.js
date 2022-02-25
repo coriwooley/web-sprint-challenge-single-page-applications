ae78cabafed81a697a865efd250316da72077ccc
Cori Wooley <wooleycori@gmail.com>
2022-02-24 20:37:38 -0800

    set up project. passed first test.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import {Route, Link} from 'react-router-dom'
+import PizzaForm from "./components/pizzaForm";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <PizzaForm />
     </>
   );
 };
---
+++ src/components/pizzaForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PizzaForm = (props) => {
+
+    return (
+        <div>
+            <form id="order-pizza">
+                Hello pizza man
+            </form>
+        </div>
+    )
+}
+
+export default PizzaForm
